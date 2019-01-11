7493c394f7f6e8e5504fed81edb496afe291d553
Juhyun Kim <juhkim111@ucla.edu>
2019-01-11 07:42:43 -0800

    add pages

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,22 @@ ENV["DOCUMENTER_DEBUG"] = "true"
 
 makedocs(
     format = :html,
-    sitename = "VarianceComponentModels",
-    modules = [VarianceComponentModels]
+    sitename = "VarianceComponentModels.jl",
+    modules = [VarianceComponentModels], 
+    pages = Any[
+        "Home" => "index.md",
+        "Manual" => Any[
+            "man/mle_reml.md",
+            "man/heritability.md"
+        ],
+        "API" => "man/api.md"
+    ]
 )
 
 deploydocs(
     repo   = "github.com/OpenMendel/VarianceComponentModels.jl.git",
-    target = "build"
-)
+    target = "build",
+    osname = "linux",
+    julia = "1.0",
+    deps = nothing,
+    make = nothing)
